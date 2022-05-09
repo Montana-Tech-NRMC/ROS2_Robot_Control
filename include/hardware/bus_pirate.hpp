77f07c7b49a87fdbf8bd4677712cc8ce99969e35
Justin Bak <tkdefender@protonmail.com>
2022-05-09 22:43:52 +0000

    command line args and fun stuff

--- include/hardware/bus_pirate.hpp
+++ include/hardware/bus_pirate.hpp
@@ -8,34 +8,45 @@
 //Read Write Block
 #define CMD_REG            0xCC
 #define MODE_REG           0x00
-#define MAX_CURRENT_H      0x01
-#define MAX_CURRENT_L      0x02
-#define CURRENT_DURATION_H 0x03
-#define CURRENT_DURATION_L 0x04
-#define DES_SPEED_A_H      0x05
-#define DES_SPEED_A_L      0x06
-#define DES_SPEED_B_H      0x07
-#define DES_SPEED_B_L      0x08
-#define DES_POS_A_H        0x09
-#define DES_POS_A_L        0x0A
-#define DES_POS_B_H        0x0B
-#define DES_POS_B_L        0x0C
+#define MAX_CURRENT_L      0x01
+#define MAX_CURRENT_H      0x02
+#define CURRENT_DURATION_L 0x03
+#define CURRENT_DURATION_H 0x04
+#define DES_SPEED_A_L      0x05
+#define DES_SPEED_A_H      0x06
+#define DES_SPEED_B_L      0x07
+#define DES_SPEED_B_H      0x08
+#define DES_POS_A_L        0x09
+#define DES_POS_A_H        0x0A
+#define DES_POS_B_L        0x0B
+#define DES_POS_B_H        0x0C
 
 #define READONLY           0x20
 /// Read only Registers
-#define POSITION_A_H       0x20
-#define POSITION_A_L       0x21
-#define POSITION_B_H       0x22
-#define POSITION_B_L       0x23
-#define ADC_A_H            0x24
-#define ADC_A_L            0x25
-#define ADC_B_H            0x26
-#define ADC_B_L            0x27
-#define ADC_C_H            0x28
-#define ADC_C_L            0x29
+#define POSITION_A_L       0x20
+#define POSITION_A_H       0x21
+#define POSITION_B_L       0x22
+#define POSITION_B_H       0x23
+#define ADC_A_L            0x24
+#define ADC_A_H            0x25
+#define ADC_B_L            0x26
+#define ADC_B_H            0x27
+#define ADC_C_L            0x28
+#define ADC_C_H            0x29
 #define SWITCH_STATES      0x2A
 #define REMEX_STATE        0x2F
 
+#define DESIRED_SPEED_A DES_SPEED_A_L
+#define DESIRED_SPEED_B DES_SPEED_B_L
+#define DESIRED_POSITION_A DES_POS_A_L
+#define DESIRED_POSITION_B DES_POS_B_L
+#define CURRENT_DURATION CURRENT_DURATION_L 
+#define MAX_CURRENT MAX_CURRENT_L 
+#define POSITION_A POSITION_A_L
+#define POSITION_B POSITION_B_L
+#define ADC_A ADC_A_L
+#define ADC_B ADC_B_L 
+#define ADC_C ADC_C_L
 
 /* Global includes */
 #include <unistd.h>
@@ -54,17 +65,47 @@ public:
     ~BusPirate();
 
 private:
+
+    int startServer();
+
+    void setMotorSpeedA(__u16 speed);
+
+    void setMotorSpeedB(__u16 speed);
+
+    void setDesiredPositionA(__u16 angle);
+
+    __u16 getDesiredPositionA();
+
+    __u8 getRemexStatus();
+
+    __u8 getRemexSwitchStates();
+
+    __u8 getRemexMode();
+
+    __u16 readADCA();
+
+    __u16 readADCB();
+
+    __u16 readADCC();
+
+    __u16 getMotorPositionA();
+
+    __u16 getMotorPositionB();
+
     void pollDevices();
 
+    int setTarget(__u8 slave_addr);
+
+    // deprecate
     int i2cReadByte(__u8 reg);
 
     int i2cReadWord(__u8 reg);
 
     int checkStatus();
 
-    int i2cWriteByte(__u8 reg, __u8 data);
+    int i2cWriteWord(__u8 reg, __u16 data);
 
-    int setTarget(__u8 slave_addr);
+    int i2cWriteByte(__u8 reg, __u8 data);
 
     int fd;
 
--- src/bus_pirate.cpp
+++ src/bus_pirate.cpp
@@ -1,11 +1,24 @@
 #include "hardware/bus_pirate.hpp"
 #include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
 
 extern "C" {
     #include <linux/i2c-dev.h>
     #include <linux/i2c.h>
     #include <i2c/smbus.h>
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
 }
+
+int BusPirate::startServer() {
+    return 0;
+}
+
+int angle;
+int command;
 
 BusPirate::BusPirate() : Node("bus_pirate") {
     devices_ = {DEVICE_A, DEVICE_B};
@@ -15,28 +28,60 @@ BusPirate::BusPirate() : Node("bus_pirate") {
         RCLCPP_ERROR(this->get_logger(), "Cannot open i2c bus");
         exit(1);
     }
-
     setTarget(DEVICE_B);
 
-    i2cWriteByte(0x00, 0x44);
-    
-    i2cWriteByte(0x01, 0x55);
+    setDesiredPositionA(angle);
 
-    int val = i2cReadWord(0x00);
+    RCLCPP_INFO(this->get_logger(), "set desired position to %d", angle);
 
-    RCLCPP_INFO(this->get_logger(), "read val %x", val);
+    unsigned int despos = getDesiredPositionA();
 
-    //timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(BusPirate::pollDevices, this));
+    RCLCPP_INFO(this->get_logger(), "des pos read %d", despos);
+
+    RCLCPP_INFO(this->get_logger(), "command sent %x", command);
+
+    i2cWriteByte(0xCC, command);
+
+/*
+    unsigned int val = i2cReadWord(DESIRED_SPEED_A);
+
+    RCLCPP_INFO(this->get_logger(), "read val %d", val);
+    int position = getMotorPositionA();
+
+    RCLCPP_INFO(this->get_logger(), "read position %d", position);
+
+*/
 }
+
+void BusPirate::setMotorSpeedA(__u16 speed) {
+    i2cWriteWord(DES_SPEED_A_L, speed);
+}
+
+void BusPirate::setMotorSpeedB(__u16 speed) {
+    i2cWriteWord(DES_SPEED_B_L, speed);
+}
+
+void BusPirate::setDesiredPositionA(__u16 angle) {
+    i2cWriteWord(DESIRED_POSITION_A, angle);
+    //return i2cReadWord(DESIRED_POSITION_A);
+}
+
+__u16 BusPirate::getDesiredPositionA() {
+    return i2cReadWord(DESIRED_POSITION_A);
+}
+
+__u16 BusPirate::getMotorPositionA() {
+    return i2cReadWord(POSITION_A_L);
+}
+
 
 BusPirate::~BusPirate() {
     close(fd);
 }
 
 void BusPirate::pollDevices() {
-    for (int dev : devices_) {
-        setTarget(dev);
-    }
+    int current = i2cReadWord(ADC_A_L);
+    RCLCPP_INFO(this->get_logger(), "(%d)", current); 
 }
 
 int BusPirate::checkStatus() {
@@ -60,14 +105,29 @@ int BusPirate::i2cReadWord(__u8 reg) {
     return res;
 }
 
+int BusPirate::i2cWriteWord(__u8 reg, __u16 data) {
+    return i2c_smbus_write_word_data(fd, reg, data);
+}
+
 int BusPirate::i2cWriteByte(__u8 reg, __u8 data) {
     int res = i2c_smbus_write_byte_data(fd, reg, data);
     return res;
 }
 
 int main(int argc, char** argv) {
+    if (argc > 2) {
+        std::stringstream command_ss(argv[1]);
+        std::stringstream angle_ss(argv[2]);
+        angle_ss >> angle;
+        command_ss >> command;
+    } else if (argc > 1) {
+        std::stringstream command_ss(argv[1]);
+        command_ss >> command;
+        angle = 0;
+    }
     rclcpp::init(argc, argv);
     auto node = std::make_shared<BusPirate>();
+    std::cout << angle << std::endl;
     rclcpp::spin(node);
     rclcpp::shutdown();
 }
